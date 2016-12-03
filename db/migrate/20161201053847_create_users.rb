d66e8df9b3d2c6871044f6c1b5d2fd86ab4dbe1d
sufinsha <sufinsha@qburst.com>
2016-12-03 09:25:05 +0530

    # adding has_secure_token method in user model # rubocop.yml edit

--- db/migrate/20161201053847_create_users.rb
+++ db/migrate/20161201053847_create_users.rb
@@ -1,10 +1,15 @@
+#
+# CreateUsers
+#
+# @author aby
+#
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :email
       t.string :username
-      t.string :uuid
-      t.string :authentication_token
+      t.string :uuid, unique: true
+      t.string :authentication_token, unique: true
       t.timestamps
     end
     add_index :users, :uuid
