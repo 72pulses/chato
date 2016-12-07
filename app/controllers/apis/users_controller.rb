bfaa90294befca277a38fd93d2c6b74a04529823
abygeorge <abyg@qburst.com>
2016-12-07 15:25:24 +0530

    spec fix

--- app/controllers/apis/users_controller.rb
+++ app/controllers/apis/users_controller.rb
@@ -7,8 +7,9 @@ module Apis
   class UsersController < BaseApiController
     def create
       @user = User.new(user_params)
-      @user.add_role :chat_user
+      @user.password = Devise.friendly_token(8)
       @message = if @user.save
+                   @user.add_role :chat_user
                    I18n.t('user.created')
                  else
                    @success = false
